fbe80215899681b5865322b1eac700917e0903ec
SloweNGG <90928397+SloweNGG@users.noreply.github.com>
2023-05-04 20:42:16 +0300

    Add files via upload

---
+++ unichat-course-master/src/components/chats.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { ChatEngine } from 'react-chat-engine';
+
+function App() {
+	return (
+		<ChatEngine
+			projectID='b2e267bb-eadf-45bc-b8fc-0399f5c58088'
+			userName='adam'
+			userSecret='pass1234'
+		/>
+	);
+}
+
+export default App;
